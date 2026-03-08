177a1f0dd026f86b63efefa9b9ec5e3a34b3a20e
Solomon Adetola <solomonadetola160@gmail.com>
2026-03-08 12:42:23 -0700

    added an ipynb file

--- firstclass.py
+++ firstclass.py
@@ -1,9 +1,15 @@
 name= 'ALICE'
 age= 25
-print("Hello, My name is" , name +  " and I am", age, "years old")
+# print("Hello, My name is" , name +  " and I am", age, "years old")
 
 name = input("Enter your name")
 birth_year = int(input("Enter your birth_year"))
 current_year = 2026
 age = current_year - birth_year
-print(f"HELLO!, {name} you are {age} years old")
+# print(f"HELLO!, {name} you are {age} years old")
+
+X = {
+    "name":"David",
+      "age": 10  
+     }
+print(X)
